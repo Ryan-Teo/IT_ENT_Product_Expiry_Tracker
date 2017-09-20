9fcec1b9d6a18daa9ee9e94fa6f46240b0675aa4
Harry Utomo <s3510376@student.rmit.edu.au>
2017-09-20 14:23:57 +0000

    working on another scan option

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 //= require popper
 //= require bootstrap
 //= require quagga
+//= require_tree .
 
 function toggleDiv() {
 //   document.getElementById('hidden_row').style.display = "inline-block";
@@ -29,44 +30,15 @@ function toggleDiv() {
     }
 }
 
-
-function load_quagga(){
-  if ($('#barcode-scanner').length > 0 && navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-
-    var last_result = [];
-    if (Quagga.initialized == undefined) {
-      Quagga.onDetected(function(result) {
-        var last_code = result.codeResult.code;
-        last_result.push(last_code);
-        if (last_result.length > 20) {
-          code = order_by_occurrence(last_result)[0];
-          last_result = [];
-          Quagga.stop();
-          $.ajax({
-            type: "POST",
-            url: '/products/get_barcode',
-            data: { upc: code }
-          });
-        }
-      });
-    }
-
-    Quagga.init({
-      inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        numOfWorkers: navigator.hardwareConcurrency,
-        target: document.querySelector('#barcode-scanner')  
-      },
-      decoder: {
-          readers : ['ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader']
+function order_by_occurrence(arr) {
+  var counts = {};
+  arr.forEach(function(value){
+      if(!counts[value]) {
+          counts[value] = 0;
       }
-    },function(err) {
-        if (err) { console.log(err); return }
-        Quagga.initialized = true;
-        Quagga.start();
-    });
+      counts[value]++;
+  });
 
-  }
-};
-$(document).on('turbolinks:load', load_quagga);
+  return Object.keys(counts).sort(function(curKey,nextKey) {
+      return counts[curKey] < counts[nextKey];
+  });
