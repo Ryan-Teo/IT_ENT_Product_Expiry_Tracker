5f89f10821984fa7976c6b704d4c4ca8a6feefb6
Harry Utomo <s3510376@student.rmit.edu.au>
2017-09-20 14:26:59 +0000

    working on another scan option

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,16 +29,3 @@ function toggleDiv() {
         x.style.display = 'none';
     }
 }
-
-function order_by_occurrence(arr) {
-  var counts = {};
-  arr.forEach(function(value){
-      if(!counts[value]) {
-          counts[value] = 0;
-      }
-      counts[value]++;
-  });
-
-  return Object.keys(counts).sort(function(curKey,nextKey) {
-      return counts[curKey] < counts[nextKey];
-  });
